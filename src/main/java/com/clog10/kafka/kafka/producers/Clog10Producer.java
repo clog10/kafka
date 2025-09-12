b62850d6e45ab78332b97c059d5fe74268baf332
Carlos Loaeza <carlosloaeza100598@gmail.com>
2025-09-12 17:01:30 -0600

    best performance producer

--- src/main/java/com/clog10/kafka/kafka/producers/Clog10Producer.java
+++ src/main/java/com/clog10/kafka/kafka/producers/Clog10Producer.java
@@ -1,7 +1,6 @@
 package com.clog10.kafka.kafka.producers;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -14,22 +13,25 @@ public class Clog10Producer {
     public static final Logger log = LoggerFactory.getLogger(Clog10Producer.class);
 
     public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092"); // Broker de Kafka
         props.put("acks", "all"); // Acnknoeledgment level
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("linger.ms", "10");
 
         try (Producer<String, String> producer = new KafkaProducer<>(props);) {
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < 1000000; i++) {
                 producer.send(
-                        new ProducerRecord<>("clog10-topic", String.valueOf(i).concat("clog10-key"), "clog10-value"))
-                        .get();
+                        new ProducerRecord<>("clog10-topic", String.valueOf(i).concat(": clog10-key"), "clog10-value"));
             }
             producer.flush();
-        } catch (InterruptedException | ExecutionException e) {
-            log.error("Message producer interrupted ", e);
         }
+
+        log.info("Processing time: {} ms", (System.currentTimeMillis() - startTime));
 
     }
 
